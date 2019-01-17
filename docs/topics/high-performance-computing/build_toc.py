326943d9c624942c71c33d989f45be52ff985761
Adam Boeglin <adam.boeglin@microsoft.com>
2019-01-16 16:12:03 -0800

    comments!

--- docs/topics/high-performance-computing/build_toc.py
+++ docs/topics/high-performance-computing/build_toc.py
@@ -5,13 +5,14 @@ import re
 import codecs
 
 input_file = open('index.md', 'r')
-count_lines = 0
 
+# Use regex to pull a title from a header
 def getname(line):
     pattern = re.compile('.*# (.*)')
     name = pattern.match(line)
     return name.group(1)
 
+# Generate the links used in anchors
 def overviewlink(name):
     shortname = name.lower()
     return str(shortname).replace(" ", "-")
@@ -19,6 +20,7 @@ def overviewlink(name):
 toc = ""
 toc_list = []
 
+# Iterate through every line of the index file to generate a list used for the TOC
 for line in input_file:
     # Look for an inline link
     inline_match = re.compile('.*\[(.*)\]\((.*)\).*')
@@ -32,29 +34,34 @@ for line in input_file:
     h3_match = re.compile('.*<h3>(.*)</h3>.*')
     h3_title = h3_match.match(line)
 
+    # Find all the sections
     if line.startswith("#"):
         name = getname(line)
         level = line.count("#")
         toc_list.append({'level': level, 'name': name})
+    # Look for markdown links
     elif inline_link:
         # Skip internal anchors
         if re.match('^#.*', inline_link.group(2)):
             continue
         toc_list.append({'level': level + 1, 'name': inline_link.group(1), 'href': inline_link.group(2)})
+    # Find links with <a href.., but the link title is probably in an h3, see below
     elif href_link:
         url = href_link.group(1)
         continue
+    # Grab titles from h3, we're assuming we already have the URL at this point
     elif h3_title:
         toc_list.append({'level': level + 1, 'name': h3_title.group(1), 'href': url})
+    # Anything else is not a line we know how to deal with, skip
     else:
         continue
 
 # Build the TOC
-
 for i in range(0,len(toc_list)):
     item_level = toc_list[i].get('level')
     item_name = toc_list[i]['name']
 
+    # Automatically indent based on the heading level
     if item_level == 1:
         indent = ""
     else:
@@ -65,19 +72,23 @@ for i in range(0,len(toc_list)):
         if toc_list[i+1].get('level') > item_level:
             toc += indent + "- name: " + item_name + '\n'
             toc += indent + "  items:" + "\n"
+            # Don't create an overview link for the top level item
             if item_level != 1:
                 toc += indent + "  " + "- name: Overview\n"
                 toc += indent + "  " + "  href: index.md#" + overviewlink(item_name) + "\n"
+        # Add an arror indicating external links
         elif toc_list[i].get('href'):
             if ".com" in toc_list[i].get('href'):
                 if re.match('^(.(?<!docs.microsoft.com))*?$', toc_list[i].get('href')):
                     item_name = item_name + u" ↗"
             toc += indent + "- name: " + item_name + '\n'
             toc += indent + "  href: " + toc_list[i].get('href') + "\n"
+        # Set anchors for sections within the article
         else:
             toc += indent + "- name: " + item_name + '\n'
             toc += indent + "  href: index.md#" + overviewlink(item_name) + "\n"
 
+# Write the TOC file
 output_file = codecs.open('TOC.yml', 'w', "utf-8")
 output_file.write(toc)
 output_file.close()
